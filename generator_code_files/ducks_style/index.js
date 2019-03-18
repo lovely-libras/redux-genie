65d2f3c2bde492ca98a8a772944581c49cbae765
Nick <nickpereslugoff@gmail.com>
2019-03-18 14:39:27 -0400

    made reducer generator for ducks

--- generator_code_files/ducks_style/index.js
+++ generator_code_files/ducks_style/index.js
@@ -1,19 +1,35 @@
-const chalk = require('chalk')
+const chalk = require("chalk");
 const fs = require("fs");
-const create_action_types = require('./create_action_types')
-
+const create_action_types = require("./create_action_types");
+const create_action_creators = require("./create_action_creators")
+const create_reducer = require("./create_reducer")
 
 module.exports = (model, modelName) => {
-
   try {
-    fs.writeFile(`./store/${modelName}/${modelName}_action_types.js`,
+    fs.writeFile(
+      `./store/${modelName}/${modelName}_action_types.js`,
       create_action_types(modelName.toUpperCase()),
       () => {
         console.log(chalk.yellow(`made action types for ${modelName}`));
       }
+    );
+
+    fs.writeFile(
+      `./store/${modelName}/${modelName}_action_creators.js`,
+      create_action_creators(modelName),
+      () => {
+        console.log(chalk.yellow(`made action creators for ${modelName}`));
+      }
+    );
+
+    fs.writeFile(
+      `./store/${modelName}/${modelName}_reducer.js`,
+      create_reducer(model, modelName),
+      () => {
+        console.log(chalk.yellow(`made reducer for ${modelName}`));
+      }
     )
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-
-}
+};
