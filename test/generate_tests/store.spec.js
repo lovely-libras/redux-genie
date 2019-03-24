dbb87344c9675e6ed6c47cd35f52fcb3a95cac60
Amy Kim <amesk3@gmail.com>
2019-03-23 20:07:12 -0400

    added action creator tests

--- test/generate_tests/store.spec.js
+++ test/generate_tests/store.spec.js
@@ -1,42 +1,11 @@
 import { expect } from "chai";
 import fetchMock from "fetch-mock";
 import store from "../../store/store";
-import {
-  getAll,
-  createOne,
-  getOne,
-  updateOne,
-  deleteOne
-  //} from "../../store/Campus/thunks_for_Campus";// for ducks
-} from "../../store/actions/thunks_for_Campus"; //for rails
+import assert from "assert";
+import mockStore from "redux-mock-store";
 
-const campuses = [
-  { name: "campus1", address: "not here yet" },
-  { name: "campus2" }
-];
-const newCampus = { name: "campusNew" };
-const oneCampus = { name: "campus1" };
-const oneUpdatedCampus = { name: "campus1", address: "something" };
+import nock from "../nockSetup";
 
-const URL = "/api/Dux";
-fetchMock.mock(URL, { data: campuses, status: 200 });
-
-const getOneURL = `/api/Dux/${oneCampus}`;
-fetchMock.mock(getOneURL, { data: campuses, status: 201 });
-
-const updateOneURL = `/api/Dux/${oneUpdatedCampus}`;
-fetchMock.mock(
-  updateOneURL,
-  { data: campuses, status: 203 },
-  { overwriteRoutes: false }
-);
-const { spawn } = require("child_process");
-
-const shell = command => {
-  let thisProc = spawn(command, { shell: true, stdio: "inherit" });
-
-  return thisProc;
-};
 /*
 describe("Rails model", () => {
   // beforeEach(done => {
@@ -94,7 +63,51 @@ describe("Rails model", () => {
   	*/
 // });
 
-describe("campuses", () => {
+//action creators
+import actions from "../../store/actions/actions_for_Campus"; //rails
+// import actions from "../../store/Campus/actions_for_Campus" //ducks
+
+//action constants
+import action_constants from "../../store/constants/action_constants_for_Campus";
+
+import {
+  getAll,
+  createOne,
+  getOne,
+  updateOne,
+  deleteOne
+  //} from "../../store/Campus/thunks_for_Campus";// for ducks
+} from "../../store/actions/thunks_for_Campus"; //for rails
+
+const campuses = [
+  { name: "campus1", address: "not here yet" },
+  { name: "campus2" }
+];
+const newCampus = { name: "campusNew" };
+const oneCampus = { name: "campus1" };
+const oneUpdatedCampus = { name: "campus1", address: "something" };
+
+const URL = "/api/Dux";
+fetchMock.mock(URL, { data: campuses, status: 200 });
+
+const getOneURL = `/api/Dux/${oneCampus}`;
+fetchMock.mock(getOneURL, { data: campuses, status: 201 });
+
+const updateOneURL = `/api/Dux/${oneUpdatedCampus}`;
+fetchMock.mock(
+  updateOneURL,
+  { data: campuses, status: 203 },
+  { overwriteRoutes: false }
+);
+const { spawn } = require("child_process");
+
+const shell = command => {
+  let thisProc = spawn(command, { shell: true, stdio: "inherit" });
+
+  return thisProc;
+};
+
+describe("overall integration test using thunks", () => {
   it("should get all campus in the store", async () => {
     await store.dispatch(getAll());
     const campusList = store.getState().Campus_state.CampusList;
@@ -137,3 +150,47 @@ describe("campuses", () => {
     ]);
   });
 });
+
+import {
+  getAllCampus,
+  createCampus
+} from "../../store/actions/actions_for_Campus"; //rails
+import {
+  GET_ALL_CAMPUS,
+  ADD_CAMPUS
+} from "../../store/constants/action_constants_for_Campus";
+
+describe("tests action creators", () => {
+  it("should create an action to get a campus", () => {
+    let payload = "other thing";
+    const expectedAction = {
+      type: action_constants.GET_CAMPUS,
+      payload
+    };
+    expect(actions.getCampus(payload)).to.deep.equal(expectedAction);
+  });
+
+  it("should create an action to get all campuses", () => {
+    let payload = "something";
+    const expectedAction = {
+      type: action_constants.GET_ALL_CAMPUS,
+      payload
+    };
+    expect(actions.getAllCampus(payload)).to.deep.equal(expectedAction);
+  });
+
+  it("should create an action to add a campus", () => {
+    let payload = "another thing";
+    const expectedAction = {
+      type: action_constants.ADD_CAMPUS,
+      payload
+    };
+    expect(actions.createCampus(payload)).to.deep.equal(expectedAction);
+  });
+});
+
+describe("thunk returns a function", () => {});
+
+describe("tests reducers. A reducer should return the new state after applying the action to the previous state, and that's the behavior tested below.", () => {});
+
+describe("tests logging middleware", () => {});
