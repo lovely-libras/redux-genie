ae1b5eb0ad72e69684cab6ad6da9113f523b677a
Ardison-Gardner <greg.ardison.gardner@gmail.com>
2019-03-25 01:47:21 -0400

    Hotfix: Cleanup.

--- cli.js
+++ cli.js
@@ -8,7 +8,10 @@ let command = input._[2];
 let arg1 = input._[3];
 const { makeLock, diffLock } = require('./lock');
 const simulation = require('./test_simulation');
-const archy = require('archy');
+
+const generateCall = `node ${__dirname}/generator_code_root.js`;
+const updateCall = `node ${__dirname}/updateCodeRoot.js`;
+const ls = `node ${__dirname}/ls.js`;
 
 const shell = command => {
   spawn(command, { shell: true, stdio: 'inherit' });
@@ -18,30 +21,15 @@ switch (command) {
   default:
     break;
   case 'generate':
-    let generateCall = `node ${__dirname}/generator_code_root.js`;
     shell(generateCall);
     break;
   case 'update':
-    let updateCall = `node ${__dirname}/updateCodeRoot.js`;
     shell(updateCall);
     break;
   case 'ls':
-    let ls = `node ${__dirname}/ls.js`;
     shell(ls);
     break;
 }
-
-// if (command === 'generate') {
-//   let generateCall = `node ${__dirname}/generator_code_root.js`;
-//   shell(generateCall);
-// }
-
-// if (command === 'update') {
-//   let updateCall = `node ${__dirname}/updateCodeRoot.js`;
-//   shell(updateCall);
-// }
-
-// these will only be for development
 
 if (command === 'delete' && arg1 === 'all') {
   let genieDeleteCall = `node ${__dirname}/erase_dummy_store.js`;
