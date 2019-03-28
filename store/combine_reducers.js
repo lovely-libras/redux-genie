0cb8c6214007f3ed803c812ef7fca549ad3af1fb
Ardison-Gardner <greg.ardison.gardner@gmail.com>
2019-03-28 16:39:42 -0400

    Feat: Removed store generation.

--- store/combine_reducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import Horses_state from './Horses/reducer_for_Horses'
-import CobraChicken_state from './CobraChicken/reducer_for_CobraChicken'
-
-export default combineReducers({
- Horses_state,
- CobraChicken_state,
-})
